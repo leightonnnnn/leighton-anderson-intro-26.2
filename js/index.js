c36bf20e09cc1b251c90dced9b7821be63ceff2e
leightonnnnn <leightonwanderson@gmail.com>
2026-05-09 22:45:11 -0500

    added skills section to index.html and removed lines from index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
 // Create and append a footer element to the body
 const footer = document.createElement('footer');
-//footer.textContent = '© 2026 Leighton Anderson';
-
-
 let date = new Date();
 let thisYear = date.getFullYear();
 let copyright = document.createElement('p');
