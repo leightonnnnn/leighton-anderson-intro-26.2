2b96a036b5ea136b2cd91b02ed9c25f4e68e1351
leightonnnnn <leightonwanderson@gmail.com>
2026-05-09 22:40:49 -0500

    refactored footer code

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 // Create and append a footer element to the body
 const footer = document.createElement('footer');
-footer.textContent = '© 2026 Leighton Anderson';
+//footer.textContent = '© 2026 Leighton Anderson';
+
+
+let date = new Date();
+let thisYear = date.getFullYear();
+let copyright = document.createElement('p');
+copyright.textContent = `Leighton Anderson ${thisYear} © `;
+footer.appendChild(copyright);
 document.body.appendChild(footer);
