81e0424321c719344f532a5be567496df507cdbd
leightonnnnn <leightonwanderson@gmail.com>
2026-05-09 22:30:14 -0500

    added footer using DOM manipulation

---
+++ js/index.js
@@ -0,0 +1,4 @@
+// Create and append a footer element to the body
+const footer = document.createElement('footer');
+footer.textContent = '© 2026 Leighton Anderson';
+document.body.appendChild(footer);
