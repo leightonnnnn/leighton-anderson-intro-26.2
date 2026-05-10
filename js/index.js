5a74026f5dbcb0d261bedeea69a5278f8561c4ac
leightonnnnn <leightonwanderson@gmail.com>
2026-05-09 22:49:46 -0500

    added skills to page using DOM manipulation in index.js

--- js/index.js
+++ js/index.js
@@ -6,3 +6,14 @@ let copyright = document.createElement('p');
 copyright.textContent = `Leighton Anderson ${thisYear} © `;
 footer.appendChild(copyright);
 document.body.appendChild(footer);
+
+//Add skills to skills section
+let skills = ['HTML', 'CSS', 'JavaScript'];
+let skillsSection = document.getElementById('skills');
+let skillsList = skillsSection.querySelector('ul');
+
+for (let i = 0; i < skills.length; i++){
+    let skill = document.createElement('li');
+    skill.textContent = skills[i];
+    skillsList.appendChild(skill);
+}
